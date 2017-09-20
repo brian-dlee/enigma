99f6e87c4e438393e01ec39231dc31ce67141a54
Brian Lee <briandl92391@gmail.com>
2017-09-20 13:17:13 -0700

    Renamed to enigma

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(
-    name='crypt',
+    name='enigma',
     version='0.1.1',
     description='Utility for generating and maintaining cryptographic certs and keys',
-    url='https://github.com/orionnetworksolutions/Crypt.git',
+    url='https://github.com/brian-dlee/enigma.git',
     author='Brian Lee',
     author_email='briandl92391@gmail.com',
     license='MIT',
